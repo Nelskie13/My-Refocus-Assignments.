e97a1bca005843c026bd7f5367ec4eb2efc83bf6
Jonel Capa <jonelcapa@yahoo.com>
2022-11-08 08:02:36 +0800

    Manual Testing for BMI Calculator

--- BMICalculator.js
+++ BMICalculator.js
@@ -2,17 +2,6 @@ function computedBMI(weight,heightInMeter){
     let computedBMI = weight / (heightInMeter**2);
     return computedBMI;
 }
-function BMIStatus(BMI){
-    if(BMI < 18.5){
-        console.log("Underweight 🥺");    
-    }else if((BMI > 18.5) && (BMI < 24.9)){
-        console.log("Normal 🤗");
-    }else if((BMI > 25) && (BMI < 29.9 )){
-        console.log("Overweight😰");
-    }else{
-        console.log("Obese 😱");
-    }
-}
 function testBMICalculator(){
     const result=computedBMI(75,1.73);
     const expected=25.05930702662969;
@@ -22,5 +11,4 @@ function testBMICalculator(){
         `The result ${result} is match the expected value ${expected}`
     )
 }
-console.log(computedBMI(75,1.73))
-BMIStatus(25)
+console.log(computedBMI(65,1.73))
